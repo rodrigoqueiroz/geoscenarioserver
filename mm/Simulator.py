b81f10e4b6c7f9f7b69e46e9eb33877a283e3041
Divit Sharma <dsharma5095@gmail.com>
2020-04-01 13:45:41 -0400

    small stuff

--- mm/Simulator.py
+++ mm/Simulator.py
@@ -82,7 +82,6 @@ def main():
     v1.setbehavior(btree=BT_VELKEEPING)
     sim_vehicles[vid] = v1
     
-    # TODO: ask how d works, and if every vehicle will have its own transform out of frenet
     vid = 1
     v2 = SV(id = vid, start_state = [0.0,14.0,0.0, 0.0,0.0,0.0]) #14 +- 50km/h
     v2.setbehavior(btree=BT_VELKEEPING)
