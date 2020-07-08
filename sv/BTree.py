a969a114bd8e2e7b137f520faa0dcadb694e4f9d
Ricardo Caldas <rdinizcal@gmail.com>
2020-07-08 15:51:05 +0200

    add setup with 2 blackboard (conds and maneu); randcondition -> condition; adequate maneuver with key and config; change tick behavior

--- sv/BTree.py
+++ sv/BTree.py
@@ -14,14 +14,24 @@ class BTree(object):
         #runtime
         self.tree = root 
         self.mconfig = None
+        self.bb_cond = blackboard.Client(name="Condition")
+        self.bb_maneu = blackboard.Client(name="Maneuver")
+
+    def setup(self, sim_time, vehicle_state, lane_config, vehicles, pedestrians, obstacles):
+        self.tree = self.drive_tree(sim_time, vehicle_state, lane_config, vehicles, pedestrians, obstacles)
+        self.bb_cond.register_key(key="EndPoint", access=common.Access.WRITE)
+        self.bb_cond.register_key(key="Occupied", access=common.Access.WRITE)
+        self.bb_cond.register_key(key="Stopped", access=common.Access.WRITE)
+        self.bb_maneu.register_key(key="key", access=common.Access.READ)
+        self.bb_maneu.register_key(key="config", access=common.Access.READ)
 
     def tick(self, sim_time, vehicle_state, lane_config, vehicles, pedestrians, obstacles):
-        mconfig = getattr(self, self.tree)(sim_time, vehicle_state, lane_config, vehicles, pedestrians, obstacles)
-        
-        self.mconfig = mconfig #tick behavior tree here! 
+        self.tree.tick_once()
+        # TODO: rethink this!
+        mkey = eval(self.bb_maneu.key)
+        self.mconfig = eval(self.bb_maneu.config)
         return self.mconfig
-        
-    
+
     def drive_tree(self, sim_time, vehicle_state, lane_config, vehicles, pedestrians, obstacles):
         ''' Driving on route and alternating between 
         velocity keeping if road ahead is free
@@ -30,14 +40,15 @@ class BTree(object):
         stop if reacthed stopping point
         '''
         ## Maneuvers List
-        free_lane = Maneuver("Velocity Keeping")
-        follow_obstc = Maneuver("Follow Vehicle")
-        stop_ego = Maneuver("Stop")
+        # TODO: Update these with the correct maneuver model configs
+        free_lane = Maneuver("M_VELKEEP", "MVelKeepConfig(MP(18.0,10,6), MP(5))")
+        follow_obstc = Maneuver("FollowVehicle", "MFollowVehicle()")
+        stop_ego = Maneuver("M_STOP", "MStop()")
         
         ## Conditions List
-        occupied = RandCondition("Occupied?")
-        end_point = RandCondition("End Point?")
-        stp_vehicle = RandCondition("Stopped?")
+        end_point = Condition("EndPoint")
+        occupied = Condition("Occupied")
+        stp_vehicle = Condition("Stopped")
 
         ## Coordinate Maneuvers and Conditions
         stp_obstc = composites.Sequence(["Stopped Obstacle"])
@@ -58,8 +69,6 @@ class BTree(object):
         root = composites.Selector("Simple Drive")
         root.add_children([reached_end_point, traj_follow])
 
-        drive_tree = trees.BehaviourTree(root=root) 
-
         drive_tree = trees.BehaviourTree(root=root)
         drive_tree.setup(timeout=15)
 
@@ -67,7 +76,6 @@ class BTree(object):
         print(display.unicode_tree(root=root))
 
         return drive_tree
-        
 
     def lanechange_tree(self, sim_time, vehicle_state, lane_config, vehicles, pedestrians, obstacles):
         #print('lane change tree')
@@ -92,13 +100,3 @@ class BTree(object):
             return self.lanechange_tree(sim_time, vehicle_state, lane_config, vehicles, pedestrians, obstacles)
         else:
             return self.drive_tree(sim_time, vehicle_state, lane_config, vehicles, pedestrians, obstacles)
-        
-
-
-    
-
-    
-
-
-
-      
