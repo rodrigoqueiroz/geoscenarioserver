5dbadb679a61658178761ba950b98c4c67acb013
Ricardo Caldas <rdinizcal@gmail.com>
2020-07-17 14:32:58 +0200

    minor fix

--- sv/btree/BTreeFactory.py
+++ sv/btree/BTreeFactory.py
@@ -4,6 +4,8 @@
 from py_trees import *
 from sv.btree.BTree import *
 from sv.btree.DriveAimlesslyTree import *
+from sv.btree.DrivePatientlyTree import *
+from sv.btree.DriveImpatientlyTree import *
 from sv.btree.LaneChangeTree import *
 from sv.btree.DriveSubtree import *
 
@@ -17,16 +19,14 @@ class BTreeFactory(object):
         self.goal = goal
     
     def build_tree(self):
-        if self.name == "lanechange":
-            tree = LaneChangeTree(self.vid, self.goal)
-        elif self.name == "drive":
-            tree = DriveSubtree(self.vid, self.goal)
+        if self.name == "impatient_drive" or self.name == "drive_timed_lane_change":
+            tree = DriveImpatientlyTree(self.vid, self.goal)
         elif self.name == "drive" or self.name == "patient_driver" or self.name=="simple_drive":
-            tree = DriveAimlesslyTree(self.vid, self.goal)
+            tree = DrivePatientlyTree(self.vid, self.goal)
         elif self.name == "alice" or self.name == "aimless_driver":
             tree = DriveAimlesslyTree(self.vid, self.goal)
         elif self.name == "default":
-            tree = DriveSubtree(self.vid, self.goal)
+            tree = DrivePatientlyTree(self.vid, self.goal)
         else:
             raise RuntimeError("Could not set Vehicle " + self.vid + " up behavior.")
             
