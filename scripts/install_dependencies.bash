ed16a87cd4afcf7dd0ec1f37193543a80078579d
Michal Antkiewicz <michal.antkiewicz@uwaterloo.ca>
2021-02-05 09:23:05 -0500

    use apt-get and -qq

--- scripts/install_dependencies.bash
+++ scripts/install_dependencies.bash
@@ -13,7 +13,7 @@ main()
 install_python_dependencies()
 {
     # ensure we have pip3
-    sudo apt install python3-pip python3-tk python3-sysv-ipc
+    sudo apt-get install -qq python3-pip python3-tk python3-sysv-ipc
     sudo pip3 install numpy scipy glog matplotlib
 }
 
@@ -21,15 +21,15 @@ install_lanelet2()
 {
     sudo apt-get -qq install libpugixml-dev
     UBUNTU_DISTRO=$(lsb_release -s -c)
-    if [[ "$UBUNTU_DISTRO" == "xenial" ]]; then
+    if [[ "${UBUNTU_DISTRO}" == "xenial" ]]; then
         # For Xenial, get the submodules and will build
         git submodule update --init --recursive
-    elif [[ "$UBUNTU_DISTRO" == "bionic" ]]; then
+    elif [[ "${UBUNTU_DISTRO}" == "bionic" ]]; then
         sudo apt-get -qq install "ros-melodic-mrt-cmake-modules" "ros-melodic-lanelet2"
-    elif [[ "$UBUNTU_DISTRO" == "focal" ]]; then
+    elif [[ "${UBUNTU_DISTRO}" == "focal" ]]; then
         sudo apt-get -qq install "ros-noetic-mrt-cmake-modules" "ros-noetic-lanelet2"
     else
-        echo "Unsupported ROS "
+        echo "Unsupported Ubuntu version ${UBUNTU_DISTRO}."
     fi
 }
 
