d9cbb05a3b7295a0d8fba4ba748388bede9586f2
Michal Antkiewicz <michal.antkiewicz@uwaterloo.ca>
2021-02-05 16:28:01 -0500

    just install python3.8 but not make it default python3 anymore. Install python3.8-dev required by sysv-ipc.

--- scripts/install_dependencies.bash
+++ scripts/install_dependencies.bash
@@ -12,20 +12,8 @@ main()
 
 install_python_dependencies()
 {
-    # Ensure we have Python3.8
-    sudo apt-get install -qq python3.8 python3.8-venv
-    # Make it the default version for the system
-    # Give the user ability to switch back to Python3.6 easily
-    sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
-    # Give Python3.8 a higher priority so that it's chosen by default
-    sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
-    # Fix 'apt update' error "ModuleNotFoundError: No module named 'apt_pkg'"
-    # Note:
-    # $ sudo apt-get install python3-apt --reinstall
-    # does not solve the error.
-    sudo ln -sfn /usr/lib/python3/dist-packages/apt_pkg.cpython-36m-x86_64-linux-gnu.so /usr/lib/python3/dist-packages/apt_pkg.so
-    # Ensure we have pip3
-    sudo apt-get install -qq python3-pip
+    # Ensure we have Python3.8 and pip3
+    sudo apt-get install -qq python3.8 python3.8-dev python3.8-venv python3-pip
     # Install the required dependencies for GeoScenario Server
     pip3 -q install numpy scipy glog matplotlib py_trees antlr4-python3-runtime tk sysv-ipc
 }
