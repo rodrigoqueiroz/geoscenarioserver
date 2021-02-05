37c9b0d424dd60dde18a5c5304bdf7cef226820e
Michal Antkiewicz <michal.antkiewicz@uwaterloo.ca>
2021-02-05 17:04:39 -0500

    add installation of Lanelet2 from source

--- scripts/install_dependencies.bash
+++ scripts/install_dependencies.bash
@@ -4,21 +4,37 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 REPO_DIR=$(dirname "$SCRIPT_DIR")
 
-main()
+activate_venv()
 {
-    install_python_dependencies
-    install_lanelet2
+    source $REPO_DIR/.venv/bin/activate
 }
 
 install_python_dependencies()
 {
     # Ensure we have Python3.8 and pip3
     sudo apt-get install -qq python3.8 python3.8-dev python3.8-venv python3-pip
+    # Create a virtual environment
+    cd $REPO_DIR
+    python3.8 -m venv .venv
+    activate_venv
     # Install the required dependencies for GeoScenario Server
     pip3 -q install numpy scipy glog matplotlib py_trees antlr4-python3-runtime tk sysv-ipc
 }
 
-install_lanelet2()
+install_lanelet2_python38()
+{
+    cd $REPO_DIR
+    activate_venv
+    mkdir -p $REPO_DIR/catkin_ws/src
+    ln -sfn $REPO_DIR/Lanelet2 $REPO_DIR/catkin_ws/src/Lanelet2
+    cd $REPO_DIR/catkin_ws
+    catkin init
+    catkin config --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DPYTHON_VERSION=3.8
+    pip3 -q install catkin_pkg rospkg
+    catkin build --workspace $REPO_DIR/catkin_ws
+}
+
+install_lanelet2_binary()
 {
     sudo apt-get -qq install libpugixml-dev
     UBUNTU_DISTRO=$(lsb_release -s -c)
@@ -34,4 +50,6 @@ install_lanelet2()
     fi
 }
 
-main
+install_python_dependencies
+install_lanelet2_python38
+install_lanelet2_binary
