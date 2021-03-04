875ce2a0492e6233f6d96576637e275d2ecab200
WAEliasmith <alex.eliasmith@uwaterloo.ca>
2021-03-04 11:41:33 -0500

    more informative print statement

--- sv/btree/BehaviorModels.py
+++ sv/btree/BehaviorModels.py
@@ -38,7 +38,7 @@ class BehaviorModels(object):
     def find_btree(self):
         for btree_path in self.btree_locations:
             if os.path.isfile(os.path.join(btree_path, self.root_btree_name)):
-                print ("Using a btree from " + btree_path)
+                print ("Using the " + self.root_btree_name + " btree from " + btree_path)
                 path,file = os.path.split(os.path.abspath(os.path.join(btree_path, self.root_btree_name)))
                 return path,file
         #Btree not found in any location
