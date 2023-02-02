17384605f7c9b56f262fde723166a23a7a6863a7
Jinwei Zhang <jinwei.zhang@uwaterloo.ca>
2023-02-01 20:38:32 -0500

    interaction dataset features fix

--- dataset_api/constants.py
+++ dataset_api/constants.py
@@ -9,14 +9,34 @@ class CONSTANTS:
         pass
 
     # Dataset constants
-    BASE_DIR = '/mnt/Data/Research_Dataset/interaction_dataset/INTERACTION-Dataset-DR-multi-v1_2/'
+    DATASET_BASE_DIR = '/mnt/Data/Research_Dataset/interaction_dataset/INTERACTION-Dataset-DR-multi-v1_2/'
     DATASET_VERSION = 'v1.2'
-    RECORDING_MAP = ['DR_DEU_Merging_MT', 'DR_DEU_Roundabout_OF']
+    RECORDING_MAP = {
+        0: 'DR_CHN_Merging_ZS0',
+        1: 'DR_CHN_Merging_ZS2',
+        2: 'DR_CHN_Roundabout_LN',
+        3: 'DR_DEU_Merging_MT',
+        4: 'DR_DEU_Roundabout_OF',
+        5: 'DR_Intersection_CM',
+        6: 'DR_LaneChange_ET0',
+        7: 'DR_LaneChange_ET1',
+        8: 'DR_Merging_TR0',
+        9: 'DR_Merging_TR1',
+        10: 'DR_Roundabout_RW',
+        11: 'DR_USA_Intersection_EP0',
+        12: 'DR_USA_Intersection_EP1',
+        13: 'DR_USA_Intersection_GL',
+        14: 'DR_USA_Intersection_MA',
+        15: 'DR_USA_Roundabout_EP',
+        16: 'DR_USA_Roundabout_FT',
+        17: 'DR_USA_Roundabout_SR'
+        }
 
     # Scene data constants
-    RECORDING = 'DR_DEU_Merging_MT'
-    MAP_DIR = BASE_DIR+'maps/'+RECORDING+'.osm'
-    CASE_ID = 10
+    RECORDING = RECORDING_MAP[3]
+    MAP_DIR = DATASET_BASE_DIR+'maps/'+RECORDING+'.osm'
+    CASE_ID = 70
+    STARTING_SIM_TIME_MS = 100
 
     # GeoScenario static icon constants
     ## Global configuration layouts, abbreviated as GC
