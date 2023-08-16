2b2f0374255a3dfd2ed298d4ac755063c5ec3622
Jinwei Zhang <jinwei.zhang@uwaterloo.ca>
2023-08-16 15:59:57 -0400

    update the INTERACTION map names as a constant

--- dataset_api/constants.py
+++ dataset_api/constants.py
@@ -11,25 +11,42 @@ class CONSTANTS:
     # Dataset constants
     DATASET_BASE_DIR = '/mnt/Data/Research_Dataset/interaction_dataset/INTERACTION-Dataset-DR-multi-v1_2/'
     DATASET_VERSION = 'v1.2'
+
+    # Older version ones
+#     RECORDING_MAP = {
+#         0: 'DR_CHN_Merging_ZS0',
+#         1: 'DR_CHN_Merging_ZS2',
+#         2: 'DR_CHN_Roundabout_LN',
+#         3: 'DR_DEU_Merging_MT',
+#         4: 'DR_DEU_Roundabout_OF',
+#         5: 'DR_Intersection_CM',
+#         6: 'DR_LaneChange_ET0',
+#         7: 'DR_LaneChange_ET1',
+#         8: 'DR_Merging_TR0',
+#         9: 'DR_Merging_TR1',
+#         10: 'DR_Roundabout_RW',
+#         11: 'DR_USA_Intersection_EP0',
+#         12: 'DR_USA_Intersection_EP1',
+#         13: 'DR_USA_Intersection_GL',
+#         14: 'DR_USA_Intersection_MA',
+#         15: 'DR_USA_Roundabout_EP',
+#         16: 'DR_USA_Roundabout_FT',
+#         17: 'DR_USA_Roundabout_SR'
+#         }
+    
     RECORDING_MAP = {
         0: 'DR_CHN_Merging_ZS0',
         1: 'DR_CHN_Merging_ZS2',
         2: 'DR_CHN_Roundabout_LN',
         3: 'DR_DEU_Merging_MT',
         4: 'DR_DEU_Roundabout_OF',
-        5: 'DR_Intersection_CM',
-        6: 'DR_LaneChange_ET0',
-        7: 'DR_LaneChange_ET1',
-        8: 'DR_Merging_TR0',
-        9: 'DR_Merging_TR1',
-        10: 'DR_Roundabout_RW',
-        11: 'DR_USA_Intersection_EP0',
-        12: 'DR_USA_Intersection_EP1',
-        13: 'DR_USA_Intersection_GL',
-        14: 'DR_USA_Intersection_MA',
-        15: 'DR_USA_Roundabout_EP',
-        16: 'DR_USA_Roundabout_FT',
-        17: 'DR_USA_Roundabout_SR'
+        5: 'DR_USA_Intersection_EP0',
+        6: 'DR_USA_Intersection_EP1',
+        7: 'DR_USA_Intersection_GL',
+        8: 'DR_USA_Intersection_MA',
+        9: 'DR_USA_Roundabout_EP',
+        10: 'DR_USA_Roundabout_FT',
+        11: 'DR_USA_Roundabout_SR'
         }
 
     # Scene data constants
@@ -37,6 +54,13 @@ class CONSTANTS:
     MAP_DIR = DATASET_BASE_DIR+'maps/'+RECORDING+'.osm'
     CASE_ID = 70
     STARTING_SIM_TIME_MS = 100
+    VEHICLE_TYPE_DICT = {
+        1: 'TV',
+        2: 'TV',
+        3: 'TV',
+        4: 'SDV',
+        5: 'SDV',
+        }
 
     # GeoScenario static icon constants
     ## Global configuration layouts, abbreviated as GC
