e2fc5ef14f3b2f56dfb633cf27bde61fffbc0ed4
Jinwei Zhang <zhangjinwei786@gmail.com>
2023-10-07 17:42:40 -0400

    adaptation change

--- dataset_api/constants.py
+++ dataset_api/constants.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # jinwei.zhang@uwaterloo.ca
 # ---------------------------------------------
-
+import os
 """This class contains the constants used in the dataset_api package."""
 class CONSTANTS:
 
@@ -9,7 +9,8 @@ class CONSTANTS:
         pass
 
     # Dataset constants
-    DATASET_BASE_DIR = '/mnt/Data/Research_Dataset/interaction_dataset/INTERACTION-Dataset-DR-multi-v1_2/'
+    DATASET_BASE_DIR = '/home/jinwei/datasets/INTERACTION-Dataset-DR-multi-v1_2/'
+    MAP_BASE_DIT = '/home/jinwei/workspace/geoscenarioserver/scenarios/maps/interaction_maps_EXT/'
     DATASET_VERSION = 'v1.2'
 
     # Older version ones
@@ -48,19 +49,36 @@ class CONSTANTS:
         10: 'DR_USA_Roundabout_FT',
         11: 'DR_USA_Roundabout_SR'
         }
+    
+    MAP_DIR_MAP = {
+        'DR_CHN_Merging_ZS0': os.path.join(MAP_BASE_DIT, 'DR_CHN_Merging_ZS0_EXT.osm'),
+        'DR_CHN_Merging_ZS2': os.path.join(MAP_BASE_DIT, 'DR_CHN_Merging_ZS2_EXT.osm'),
+        'DR_CHN_Roundabout_LN': os.path.join(MAP_BASE_DIT, 'DR_CHN_Roundabout_LN_EXT.osm'),
+        'DR_DEU_Merging_MT': os.path.join(MAP_BASE_DIT, 'DR_DEU_Merging_MT_EXT.osm'),
+        'DR_DEU_Roundabout_OF': os.path.join(MAP_BASE_DIT, 'DR_DEU_Roundabout_OF_EXT.osm'),
+        'DR_USA_Intersection_EP0': os.path.join(MAP_BASE_DIT, 'DR_USA_Intersection_EP0_EXT.osm'),
+        'DR_USA_Intersection_EP1': os.path.join(MAP_BASE_DIT, 'DR_USA_Intersection_EP1_EXT.osm'),
+        'DR_USA_Intersection_GL': os.path.join(MAP_BASE_DIT, 'DR_USA_Intersection_GL_EXT.osm'),
+        'DR_USA_Intersection_MA': os.path.join(MAP_BASE_DIT, 'DR_USA_Intersection_MA_EXT.osm'),
+        'DR_USA_Roundabout_EP': os.path.join(MAP_BASE_DIT, 'DR_USA_Roundabout_EP_EXT.osm'),
+        'DR_USA_Roundabout_FT': os.path.join(MAP_BASE_DIT, 'DR_USA_Roundabout_FT_EXT.osm'),
+        'DR_USA_Roundabout_SR': os.path.join(MAP_BASE_DIT, 'DR_USA_Roundabout_SR_EXT.osm')
+    }
 
     # Scene data constants
-    RECORDING = RECORDING_MAP[3]
-    MAP_DIR = DATASET_BASE_DIR+'maps/'+RECORDING+'.osm'
-    CASE_ID = 70
+    # RECORDING = RECORDING_MAP[7]
+    # MAP_DIR = MAP_DIR_MAP[RECORDING]
+    # MAP_DIR = DATASET_BASE_DIR+'maps/'+RECORDING+'.osm'
+    CASE_ID = 31
     STARTING_SIM_TIME_MS = 100
-    VEHICLE_TYPE_DICT = {
-        1: 'TV',
-        2: 'TV',
-        3: 'TV',
-        4: 'SDV',
-        5: 'SDV',
-        }
+    # VEHICLE_TYPE_DICT = {
+    #     1: 'TV',
+    #     2: 'TV',
+    #     3: 'TV',
+    #     4: 'SDV',
+    #     5: 'SDV',
+    #     }
+    VEHICLE_TYPE_DICT = {i: 'SDV' for i in range(1, 50)}
 
     # GeoScenario static icon constants
     ## Global configuration layouts, abbreviated as GC
