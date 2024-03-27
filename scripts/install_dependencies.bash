79f225330da2501b736e570d1e518904fb1400c4
Michał Antkiewicz <michal.antkiewicz@uwaterloo.ca>
2024-03-27 14:28:57 -0400

    upgrade pip needed for lanelet2 installation, install venv needed on Ubuntu 24.04

--- scripts/install_dependencies.bash
+++ scripts/install_dependencies.bash
@@ -13,7 +13,7 @@ echo ""
 echo "Installing Python3 packages"
 echo ""
 # Ensure we have Python3 and pip3
-sudo apt-get install -qq python3 python3-dev python3-tk python3-pip python3-pil python3-pil.imagetk
+sudo apt-get install -qq python3 python3-dev python3-tk python3-pip python3-pil python3-pil.imagetk python3-venv
 
 echo ""
 echo "Installing GraphViz"
@@ -24,6 +24,8 @@ sudo apt-get install -qq graphviz
 echo ""
 echo "Installing the required dependencies for GeoScenario Server for Python3"
 echo ""
+# required as per Note in https://pypi.org/project/lanelet2/
+python3 -m pip install -U pip
 python3 -m pip install -r ${REPO_DIR}/requirements.txt
 
 echo ""
