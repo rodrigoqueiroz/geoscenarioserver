9bb59e215e80275642f4bb254936085c77354614
Michał Antkiewicz <michal.antkiewicz@uwaterloo.ca>
2024-03-27 12:27:59 -0400

    install graphviz

--- scripts/install_dependencies.bash
+++ scripts/install_dependencies.bash
@@ -9,6 +9,8 @@ echo "Installing Python3 and other packages"
 echo ""
 # Ensure we have Python3 and pip3
 sudo apt-get install -qq python3 python3-dev python3-tk python3-pip python3-pil python3-pil.imagetk
+# dot is required
+sudo apt-get install -qq graphviz
 
 echo ""
 echo "Installing the required dependencies for GeoScenario Server for Python3"
