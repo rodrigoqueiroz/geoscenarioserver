6c75caa724bf7cf41b6a8cb2657d2fdf8852bd18
Michał Antkiewicz <michal.antkiewicz@uwaterloo.ca>
2025-02-04 14:01:39 -0500

    restore SimConfig from master

--- SimConfig.py
+++ SimConfig.py
@@ -15,17 +15,17 @@ ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 #Sim Config
 TIMEOUT = 30                #default timeout in [s] if not defined by scenario
-TRAFFIC_RATE = 20           #global tick rate
+TRAFFIC_RATE = 30           #global tick rate
 WAIT_FOR_INPUT = False      #wait for user input before starting simulation
 
 #Dash Config
 SHOW_DASHBOARD = True      	#show dash with plots, vehicles and trajectories. Optional.
-DASH_RATE = 20              #dash tick rate. Max is traffic rate.
+DASH_RATE = 10              #dash tick rate. Max is traffic rate.
 PLOT_VID = 1               	#vehicle to center the main plot around, if not defined by the scenario.
                             #Make sure there exists a vehicle with this id
 #Global Map
 SHOW_MPLOT = True           #whether to show the global map cartesian plot
-MPLOT_SIZE = 80			#map plot: road length in meters (shorter=better performance)
+MPLOT_SIZE = 60			#map plot: road length in meters (shorter=better performance)
 #Frenet Map
 SHOW_FFPLOT = True          #whether to show the frenet frame plot
 SHOW_OCCUPANCY = True       #whether to show road and intersection occupancy inside the frenet plot
@@ -34,7 +34,7 @@ FFPLOT_LENGTH = 60			#frenet frame plot: road length (s) in meters
 FFPLOT_LITE = False         #frenet frame plot: if true, plots a simplified version with onlye self vehicle. If false, plots all vehicles, trajectories and candidates
 #Cartesian
 SHOW_CPLOT = True           #whether to show the cartesian plot
-CPLOT_SIZE = 100			    #cartesian plot: road length in meters (shorter=better performance)
+CPLOT_SIZE = 60			    #cartesian plot: road length in meters (shorter=better performance)
 REFERENCE_PATH = True       #reference path indicating the frenet frame for the vehicle
 SHOW_VEHICLE_SHAPE = True   #vehicle plot with rectangle shape. 
 SHOW_VEHICLE_RADIUS = False #vehicle plot with radius.
@@ -59,8 +59,8 @@ VEHICLE_LENGTH = 4.5        #vehicle length in [m]
 VEHICLE_WIDTH = 1.8         #vehicle width in [m]
 
 #Planning
-PLANNER_RATE = 10                    #Planner tick rate
-PLANNING_TIME = 0.1                #[s] Must be <= 1/PLANNTER_RATE (we recommend 0.100 for scenarios with <4 vehicles)
+PLANNER_RATE = 3                    #Planner tick rate
+PLANNING_TIME = 0.33                #[s] Must be <= 1/PLANNTER_RATE (we recommend 0.100 for scenarios with <4 vehicles)
 USE_FIXED_PLANNING_TIME = True      #True: the plan will target PLANNING_TIME. False, the planner will vary between PLANNING_TIME and max time (1/PLANNTER_RATE)
 POINTS_PER_METER = 3.0               #The number of points per meter to be used along the vehicle's reference path
                                      #Note that the value that is used may be slightly different
