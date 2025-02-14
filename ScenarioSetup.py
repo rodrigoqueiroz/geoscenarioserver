fc46e4a08771a538d57ec27a46c0b01fb992c9a5
Frédéric Bouchard <arthmarellion@hotmail.com>
2025-02-13 19:44:46 -0500

    feat(SDV Rule-Engine): Adding Rule-Engine as a SDV

--- ScenarioSetup.py
+++ ScenarioSetup.py
@@ -23,6 +23,10 @@ from sp.Pedestrian import *
 from gsc.GSParser import GSParser
 from Actor import *
 
+def extract_tag(vnode, name, default_value, parser_fn):
+    return parser_fn(vnode.tags[name]) if name in vnode.tags else default_value
+
+
 def load_geoscenario_from_file(gsfiles, sim_traffic:SimTraffic, sim_config:SimConfig, lanelet_map:LaneletMap, map_path, btree_locations):
     """ Setup scenario from GeoScenario file
     """
@@ -59,7 +63,7 @@ def load_geoscenario_from_file(gsfiles, sim_traffic:SimTraffic, sim_config:SimCo
         map_file = os.path.join(map_path, parser.globalconfig.tags['lanelet']) #use parameter map path
     # use origin from gsc file to project nodes to sim frame
     altitude  = parser.origin.tags['altitude'] if 'altitude' in parser.origin.tags else 0.0
-
+    
     if use_local_cartesian:
         projector = LocalCartesianProjector(lanelet2.io.Origin(parser.origin.lat, parser.origin.lon, altitude))
         log.info("Using LocalCartesianProjector")
@@ -103,7 +107,7 @@ def load_geoscenario_from_file(gsfiles, sim_traffic:SimTraffic, sim_config:SimCo
             break
         vid = int(vid)   #<= must ne integer
         name = vnode.tags['name']   #vehicle name
-        model = vnode.tags['model'] if 'model' in vnode.tags else ''
+        model = extract_tag(vnode, 'model', '', str)
         start_state = [vnode.x,0.0,0.0,vnode.y,0.0,0.0]
         start_in_frenet = False
         #yaw = 90.0
@@ -114,10 +118,13 @@ def load_geoscenario_from_file(gsfiles, sim_traffic:SimTraffic, sim_config:SimCo
         #        yaw += 360.0
         #    elif yaw >= 180.0:
         #        yaw -= 360.0
-        yaw = float(vnode.tags['yaw'])*-1 if 'yaw' in vnode.tags else 0.0
         #print(yaw)
-        btype = vnode.tags['btype'].lower() if 'btype' in vnode.tags else ''
-        # log.info("Vehicle {}, behavior type {}".format(vid,btype))
+
+        btype = extract_tag(vnode, 'btype', '', str).lower()
+        rule_engine_port = extract_tag(vnode, 'rule_engine_port', None, int)
+        yaw = -extract_tag(vnode, 'yaw', 0.0, float)
+
+        log.info("Vehicle {}, behavior type {}".format(vid,btype))    
         #SDV Model (dynamic vehicle)
         if btype == 'sdv':
             #start state
@@ -160,17 +167,17 @@ def load_geoscenario_from_file(gsfiles, sim_traffic:SimTraffic, sim_config:SimCo
                 log.error("SDV {} requires a route .".format(vid))
                 continue
             gs_route = vnode.tags['route']
-            route_nodes = [ TrajNode(x = node.x, y = node.y) for node in parser.routes[gs_route].nodes ]
+            route_nodes = [ TrajNode(x = node.x, y = node.y) for node in parser.routes[gs_route].nodes ]   
             route_nodes.insert(0, TrajNode(x=vnode.x,y=vnode.y)) #insert vehicle location as start of route
             #btree
             #a behavior tree file (.btree) inside the btype's folder, defaulted in btrees
-            root_btree_name = vnode.tags['btree'] if 'btree' in vnode.tags else "st_standard_driver.btree"
+            root_btree_name = vnode.tags['btree'] if 'btree' in vnode.tags else "st_standard_driver.btree" 
             try:
                 vehicle = SDV(  vid, name, root_btree_name, start_state, yaw,
                                 lanelet_map, route_nodes,
                                 start_state_in_frenet=start_in_frenet,
                                 btree_locations=btree_locations,
-                                btype=btype
+                                btype=btype, rule_engine_port=rule_engine_port
                             )
                 #vehicle = SDV(  vid, name, root_btree_name, start_state, yaw,
                 #                lanelet_map, sim_config.lanelet_routes[vid],
@@ -213,6 +220,7 @@ def load_geoscenario_from_file(gsfiles, sim_traffic:SimTraffic, sim_config:SimCo
                         if prev_node.x_vel is None or prev_node.y_vel is None:
                             prev_node.x_vel = nd.x_vel
                             prev_node.y_vel = nd.y_vel
+                    nd.speed = float(node.tags['speed']) if ('speed' in node.tags) else None
                     nd.yaw = float(node.tags['yaw']) if ('yaw' in node.tags) else None
                     trajectory.append(nd)
                     prev_node = nd
@@ -221,9 +229,10 @@ def load_geoscenario_from_file(gsfiles, sim_traffic:SimTraffic, sim_config:SimCo
                             start_state = start_state,                      #vehicle start state in cartesian frame [x,x_vel,x_acc, y,y_vel,y_acc]
                             yaw = yaw,
                             trajectory = trajectory)                        #a valid trajectory with at least x,y,time per node
-
+                            
                 sim_traffic.add_vehicle(vehicle)
                 log.info("Vehicle {} initialized with TV behavior".format(vid))
+
             except Exception as e:
                 log.error("Failed to initialize vehicle {}".format(vid))
                 raise e
@@ -272,7 +281,7 @@ def load_geoscenario_from_file(gsfiles, sim_traffic:SimTraffic, sim_config:SimCo
         # External Vehicle (EV)
         elif btype == 'ev':
             bsource = vnode.tags['bsource']
-            vehicle = EV(vid, name, start_state, yaw, bsource)
+            vehicle = EV(vid, name, start_state, yaw, bsource) 
             vehicle.model = model
             sim_traffic.add_vehicle(vehicle)
             log.info("Vehicle {} initialized as an external vehicle".format(vid))
---
+++ sv/ruleEngine/BehaviorLayer.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python3
+#fbouchar@uwaterloo.ca
+import dataclasses
+import json
+import numpy as np
+import requests
+import sys
+
+from copy      import deepcopy
+from decimal   import Decimal
+from functools import partial
+
+from sv.ManeuverConfig import *
+from sv.ruleEngine.constants import *
+
+def flatten(props, keys):
+    if props == None or len(props) != len(keys):
+        return None
+
+    my_props = {}
+
+    for index, key in enumerate(keys):
+        my_props[key] = props[index]
+
+    return my_props
+
+def to_cartesian_space(props):
+    return flatten(props, keys=['x', 'y'])
+
+def to_frenet_frame(props):
+    return flatten(props, keys=['s', 'd'])
+
+class NanConverter(json.JSONEncoder):
+    def encode(self, obj, *args, **kwargs):
+        return super().encode(self.nan2None(obj), *args, **kwargs)
+
+    def nan2None(self, obj):
+        #if isinstance(obj, InferredProposition):
+        #    return { key: self.nan2None(value) for key, value in obj.__dict__.items() }
+
+        if isinstance(obj, dict):
+            return { key: self.nan2None(value) for key, value in obj.items() }
+
+        elif isinstance(obj, (list, tuple)):
+            return [ self.nan2None(value) for value in obj ]
+
+        elif (isinstance(obj, float) or isinstance(obj, Decimal)) and np.isnan(obj):
+            return None
+
+        return obj
+
+class BehaviorLayer(object):
+    ''''
+        Behavior Layer using external rule engine 3.0 as core implementation.
+        This module communicates with an external rule-based system at every tick.
+        Inputs: Traffic State (vehicle state, lane config, traffic)
+        Outputs: Maneuver (mconfig), and ref path changed (if reference path has changed)
+    '''
+
+    def __init__(self, vid, btype, port):
+        self.btype = btype
+        self.vid = vid
+
+        #Runtime status:
+        self._traffic_state    = None
+
+        #decision
+        self._current_mconfig  = None
+        self._ref_path_changed = False
+
+        # Communication
+        self.debug  = True
+        self.host   = "localhost"
+        self.port   = port
+        self.server = 'http://' + self.host + ':' + str(self.port)
+
+        self.session = requests.Session()
+        self.headers = {
+            'Authorization': 'CarlaMoose',
+            'Content-Type': 'application/json'
+        }
+
+        self.iteration = 0
+        self.last_behaviour = {
+            "maneuver": "EMERGENCY_STOP"
+        }
+
+    def communicate(self, method, uri, callback, data=None):
+        try:
+            prepped = self.session.prepare_request(requests.Request(method, self.server + uri, data=data, headers=self.headers))
+            response = self.session.send(prepped, timeout=0.5)
+            
+            if 'application/json' in response.headers.get('Content-Type', ''):
+                return response.json()
+
+            return response.text
+
+        except requests.exceptions.RequestException as e:
+            print('RE, RequestException:', e)
+
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+
+        # Just retry
+        return callback()
+
+    def deactivate_logs(self):
+        return self.communicate('GET', '/deactivateLog', self.deactivate_logs)
+
+    def flush(self):
+        return self.communicate('GET', '/flush', self.flush)
+
+    def get_traffic_state(self):
+        return self._traffic_state
+
+    def post_behaviour(self, situation):
+        return self.communicate('POST', '/behaviour', partial(self.post_behaviour, situation), data=json.dumps(situation, cls=NanConverter))
+
+    def set_maneuver(self, mconfig):
+        self._current_mconfig = mconfig
+
+    def set_ref_path_changed(self, val):
+        self._ref_path_changed = val
+
+    def tick(self, traffic_state):
+        self.iteration += 1 
+        self._ref_path_changed = False
+        self._traffic_state = traffic_state
+
+        situation = deepcopy(traffic_state)
+        situation.simulation = {
+            "sim_time": traffic_state.sim_time,
+            "tick": self.iteration
+        }
+
+        del situation.sim_time
+
+        if situation.route_complete:
+            situation.goal_point        = to_cartesian_space(traffic_state.goal_point)
+            situation.goal_point_frenet = to_frenet_frame(traffic_state.goal_point_frenet)
+        else:
+            situation.goal_point        = None
+            situation.goal_point_frenet = None
+
+        def convert_to_dict(data_class, banned_keys = []):
+            dto    = data_class.__dict__
+            to_ban = np.array(banned_keys).view().tolist()
+
+            for key in list(dto):
+                if key == '_left_lane':
+                    to_ban.append('_right_lane')
+                elif key == '_right_lane':
+                    to_ban.append('_left_lane')
+
+                if key in banned_keys:
+                    del dto[key]
+                elif dataclasses.is_dataclass(dto[key]):
+                    dto[key] = convert_to_dict(dto[key], to_ban)
+                elif isinstance(dto[key], list):
+                    dto[key] = {}
+
+                    for index in range(len(dto[key])):
+                        dto[key]['i_' + index] = convert_to_dict(dto[key], to_ban)
+
+            return dto
+
+        # Temporary overrides
+        situation.traffic_vehicles = {}
+        situation.traffic_vehicles_orp = {}
+
+        #parameters['situation']['last_behaviour'] = self.last_behaviour
+        self.last_behaviour = self.post_behaviour(convert_to_dict(situation))
+
+        if self.debug:
+            print('Behaviour', self.last_behaviour)
+
+        # Default Behaviour
+        self._current_mconfig = MStopConfig( target=MStopConfig.StopTarget.NOW )
+
+        if 'type' in self.last_behaviour['maneuver']:
+
+            # Track-Speed behaviour
+            if self.last_behaviour['maneuver']['type'] == 'TRACK_SPEED' and \
+               'speed' in self.last_behaviour['parameters']:
+                desired_speed = self.last_behaviour['parameters']['speed'] / KMH_TO_MS
+                self._current_mconfig = MVelKeepConfig(vel=MP(desired_speed, 10, 6))
+
+            # Stop-At behaviour
+            elif self.last_behaviour['maneuver']['type'] == 'STOP_AT':
+                # Stop-At Goal
+                if 'location' in self.last_behaviour['parameters'] and\
+                   's' in self.last_behaviour['parameters']['location']:
+                    self._current_mconfig = MStopConfig( 
+                        target=MStopConfig.StopTarget.S_POS, 
+                        pos=self.last_behaviour['parameters']['location']['s']
+                    )
+
+        return self._current_mconfig, self._ref_path_changed, ""
