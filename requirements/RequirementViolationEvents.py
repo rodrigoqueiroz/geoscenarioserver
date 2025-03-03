1e35f39c7dda7d0b68d154a82751a568498c2ad3
Frédéric Bouchard <arthmarellion@hotmail.com>
2025-03-03 11:47:45 -0500

    feat(collisionWithVehicle): Collision with a vehicle in violation report (#139)
    * feat(collisionWithVehicle): Collision with a vehicle in violation report
    
    * fix(collisionFilter): Improving the collision filter
    
    * Providing collider_id when a CollisionWithVehicle occurs
    
    * Providing colliderId when a CollisionWithVehicle occurs

--- requirements/RequirementViolationEvents.py
+++ requirements/RequirementViolationEvents.py
@@ -6,10 +6,11 @@ from multiprocessing import Manager, Value
 manager = Manager()
 
 # Singleton
-agent_ticks  = manager.dict()
-file_name    = os.getenv('VIOLATION_REPORT_FOLDER', './results') + '/violations.json'
-global_tick  = Value('i', -1)
-violations   = manager.dict()
+agent_collisions = manager.dict()
+agent_ticks      = manager.dict()
+file_name        = os.getenv('VIOLATION_REPORT_FOLDER', './results') + '/violations.json'
+global_tick      = Value('i', -1)
+violations       = manager.dict()
 
 # Generic
 class ScenarioEnd:
@@ -19,14 +20,14 @@ class ScenarioEnd:
 			file.write(json.dumps(violations.copy()))
 
 class UnmetRequirement:
-	def raise_it(self, agent_id, message):
+	def raise_it(self, agent_id, payload):
 		agent_logs = violations[agent_id]
 		agent_tick = agent_ticks[agent_id]
 
 		if agent_tick not in agent_logs:
 			agent_logs[agent_tick] = {}
 
-		agent_logs[agent_tick][self.__class__.__name__] = message
+		agent_logs[agent_tick][self.__class__.__name__] = payload
 		
 		# That reassignment is necessary for the update to work in multiprocessing
 		violations[agent_id] = agent_logs
@@ -36,16 +37,28 @@ class UnmetRequirement:
 class AgentTick:
 	def __init__(self, agent_id):
 		if agent_id not in agent_ticks:
-			agent_ticks[agent_id] = -1
-			violations[agent_id]  = {}
+			agent_collisions[agent_id] = {}
+			agent_ticks[agent_id]      = -1
+			violations[agent_id]       = {}
 
 		agent_ticks[agent_id] += 1
 
+
 class CollisionWithVehicle(UnmetRequirement):
 	def __init__(self, agent_id, vid):
-		self.raise_it(agent_id,
-			'v' + str(agent_id) + ' bounding box overlapped with the vehicle agent v' + str(vid)
-		)
+		collision_state = agent_collisions[agent_id]
+
+		# There must be a gap of 5 ticks without collision between collision with the same agent
+		if vid not in collision_state or agent_ticks[agent_id] - 5 > collision_state[vid]:
+			self.raise_it(agent_id, {
+				'colliderId': vid,
+				'message': 'v' + str(agent_id) + ' bounding box overlapped with the vehicle agent v' + str(vid)
+			})
+
+		collision_state[vid] = agent_ticks[agent_id]
+
+		# That reassignment is necessary for the update to work in multiprocessing
+		agent_collisions[agent_id] = collision_state
 
 class GlobalTick:
 	def __init__(self):
@@ -55,9 +68,9 @@ class GlobalTick:
 
 class GoalOvershot(UnmetRequirement):
 	def __init__(self, agent_id):
-		self.raise_it(agent_id,
-			'v' + str(agent_id) + ' drove past its target location'
-		)
+		self.raise_it(agent_id, {
+			'message': 'v' + str(agent_id) + ' drove past its target location'
+		})
 
 class ScenarioCompletion(Exception):
     pass
@@ -65,10 +78,10 @@ class ScenarioCompletion(Exception):
 class ScenarioTimeout(UnmetRequirement):
 	def __init__(self, timeout):
 		for agent_id in agent_ticks:
-			self.raise_it(agent_id,
-				'v' + str(agent_id) + ' did not reach its target location during the ' 
-				    + str(timeout) + ' seconds allowed by this scenario.'
-			)
+			self.raise_it(agent_id, {
+				'message': 'v' + str(agent_id) + ' did not reach its target location during the ' 
+				    		   + str(timeout) + ' seconds allowed by this scenario.'
+			})
 		ScenarioEnd()
 
 # Autoclean
--- requirements/RequirementsChecker.py
+++ requirements/RequirementsChecker.py
@@ -1,18 +1,70 @@
-from requirements.RequirementViolationEvents import GoalOvershot, ScenarioCompletion, ScenarioEnd
+import numpy as np
+
+from math import cos, radians, sin
+
+from Actor import ActorSimState
+from requirements.RequirementViolationEvents import CollisionWithVehicle, GoalOvershot, ScenarioCompletion, ScenarioEnd
 from sv.SDVTrafficState import *
 
 class RequirementsChecker:
-	def __init__(self, goal_ends_simulation):
+	def __init__(self, ego_vehicle, goal_ends_simulation):
 		self.conditions = [
+			self.detect_collisions,
 			self.detect_goal_overshot
 		]
 
+		self.ego_vehicle = ego_vehicle
 		self.goal_ends_simulation = goal_ends_simulation
 		self.goal_s_distance = float('inf')
+		self.vehicles = {}
 
 	def analyze(self, traffic_state:TrafficState):
+		self.vehicles = {}
+		self.vehicles.update(traffic_state.traffic_vehicles)
+		self.vehicles.update(traffic_state.traffic_vehicles_orp)
+
 		for condition in self.conditions:
 			condition(traffic_state)
+
+	def calculate_rectangular_bounding_box(self, vehicle):
+		center_x     = vehicle.state.x
+		center_y     = vehicle.state.y
+		half_length  = vehicle.bounding_box_length / 2
+		half_width   = vehicle.bounding_box_width  / 2
+		yaw          = -vehicle.state.yaw
+
+		bottom_left  = self.rotate(center_x, center_y, center_x - half_length, center_y - half_width, yaw)
+		bottom_right = self.rotate(center_x, center_y, center_x + half_length, center_y - half_width, yaw)
+		top_left     = self.rotate(center_x, center_y, center_x - half_length, center_y + half_width, yaw)
+		top_right    = self.rotate(center_x, center_y, center_x + half_length, center_y + half_width, yaw)
+
+		return np.array([top_left, top_right, bottom_right, bottom_left])
+
+	def detect_collisions(self, traffic_state:TrafficState):
+		ego_vehicle       = self.ego_vehicle
+
+		if ego_vehicle.sim_state is not ActorSimState.ACTIVE:
+			return
+
+		ego_box           = self.calculate_rectangular_bounding_box(ego_vehicle)
+
+		ego_min_x, ego_min_y = np.min(ego_box, axis=0)
+		ego_max_x, ego_max_y = np.max(ego_box, axis=0)
+
+		for vid, vehicle in self.vehicles.items():
+			if vehicle.sim_state not in [ ActorSimState.ACTIVE, ActorSimState.ACTIVE.value]:
+				continue
+
+			max_offset = max(vehicle.bounding_box_width / 2, vehicle.bounding_box_length / 2)
+
+			# To avoid O(v²) comparisons and favor O(v) comparisons whenever possible.
+			# Suppose big rectangles were overshoting the real dimension of the vehicles,
+			# Would they overlap? If yes, then go over the polygons and make the true comparison.
+			if (ego_min_x - max_offset <= vehicle.state.x <= ego_max_x + max_offset) and \
+			   (ego_min_y - max_offset <= vehicle.state.y <= ego_max_y + max_offset):
+				vehicle_box = self.calculate_rectangular_bounding_box(vehicle)
+				if self.do_polygons_intersect(ego_box, vehicle_box):
+					CollisionWithVehicle(ego_vehicle.id, vid)
 
 
 	def detect_goal_overshot(self, traffic_state:TrafficState):
@@ -50,3 +102,67 @@ class RequirementsChecker:
 			if self.goal_ends_simulation:
 				ScenarioEnd()
 				raise ScenarioCompletion()
+
+	def do_polygons_intersect(self, polygon_a, polygon_b):
+		"""
+		* Reference: https://stackoverflow.com/questions/10962379/how-to-check-intersection-between-2-rotated-rectangles
+		* Determine whether there is an intersection between the two polygons described
+		* by the lists of vertices. Uses the Separating Axis Theorem
+		*
+		* @param a an ndarray of connected points [[x_1, y_1], [x_2, y_2],...] that form a closed polygon
+		* @param b an ndarray of connected points [[x_1, y_1], [x_2, y_2],...] that form a closed polygon
+		* @return true if there is any intersection between the 2 polygons, false otherwise
+		"""
+		polygons = [polygon_a, polygon_b];
+
+		for polygon in polygons:
+			# for each polygon, look at each edge of the polygon, and determine if it separates the two shapes
+			for edge_index in range(len(polygon)):
+
+				# grab 2 vertices to create an edge
+				edge_index_2 = (edge_index + 1) % len(polygon);
+				[ x1, y1 ] = polygon[edge_index];
+				[ x2, y2 ] = polygon[edge_index_2];
+
+				# find the line perpendicular to this edge
+				normal = { 'x': y2 - y1, 'y': x1 - x2 };
+
+				minA, maxA = None, None
+				# for each vertex in the first shape, project it onto the line perpendicular to the edge
+				# and keep track of the min and max of these values
+				for vertex in polygon_a:
+					[vx, vy] = vertex
+					projected = normal['x'] * vx + normal['y'] * vy;
+					
+					if (minA is None) or (projected < minA):
+						minA = projected
+
+					if (maxA is None) or (projected > maxA):
+						maxA = projected
+
+				# for each vertex in the second shape, project it onto the line perpendicular to the edge
+				# and keep track of the min and max of these values
+				minB, maxB = None, None
+				for vertex in polygon_b:
+					[vx, vy] = vertex
+					projected = normal['x'] * vx + normal['y'] * vy
+
+					if (minB is None) or (projected < minB):
+						minB = projected
+
+					if (maxB is None) or (projected > maxB):
+						maxB = projected
+
+				# if there is no overlap between the projects, the edge we are looking at separates the two
+				# polygons, and we know there is no overlap
+				if (maxA < minB) or (maxB < minA):
+					return False;
+
+		return True
+
+	def rotate(self, center_x, center_y, x, y, degree_theta):
+		radian_theta = radians(degree_theta)
+		x2 = center_x + (x - center_x) * cos(radian_theta) + (y - center_y) * sin(radian_theta)
+		y2 = center_y - (x - center_x) * sin(radian_theta) + (y - center_y) * cos(radian_theta)
+
+		return np.array([ x2, y2 ])
