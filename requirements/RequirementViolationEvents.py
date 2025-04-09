77b4efb517add5f547b0fff8be7113af88f8d690
Charlie Zheng <72936974+zhengc84@users.noreply.github.com>
2025-04-09 13:21:55 -0400

    migrated pedestrian collision check from planner to SimTraffic (#156)
    * migrated pedestrian collision check to trafficTicks isntead of planner
    
    * vehicle interaction added velocity vraiable
    
    * removed debug print statements
    
    * write performance log to outputs/ instead of log/ subfolder
    
    ---------
    
    Co-authored-by: Michal Antkiewicz <michal.antkiewicz@uwaterloo.ca>

--- requirements/RequirementViolationEvents.py
+++ requirements/RequirementViolationEvents.py
@@ -12,7 +12,7 @@ file_name        = os.path.join(os.getenv("GSS_OUTPUTS", os.path.join(os.getcwd(
                                 "violations.json")
 global_tick      = Value('i', -1)
 violations       = manager.dict()
-TICKS_REQUIRED_WITHOUT_OVERLAPING_THIS_ACTOR = 5
+TICKS_REQUIRED_WITHOUT_OVERLAPING_THIS_ACTOR = 7
 
 # Generic
 class ScenarioEnd:
@@ -65,7 +65,6 @@ class CollisionWithVehicle(UnmetRequirement):
 class CollisionWithPedestrian(UnmetRequirement):
 	def __init__(self, agent_id, pid, collision_zone, relative_angle):
 		collision_state = agent_collisions[agent_id]
-	
 
 		if pid not in collision_state or agent_ticks[agent_id] - TICKS_REQUIRED_WITHOUT_OVERLAPING_THIS_ACTOR > collision_state[pid]:
 			self.raise_it(agent_id, {
