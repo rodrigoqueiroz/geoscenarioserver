3888a0ce8e958ee06b0da4bb857b040556132e07
Michal Antkiewicz <michal.antkiewicz@uwaterloo.ca>
2025-10-31 16:36:28 -0400

    added the skeleton of the ROS2 native server based on the ROS2 client

--- scripts/pixi_build_ros2_client.bash
+++ scripts/pixi_build_ros2_client.bash
@@ -4,5 +4,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 REPO_DIR=$(dirname "$SCRIPT_DIR")
 mkdir -p ${REPO_DIR}/colcon_ws/src
 ln -sfn ${REPO_DIR}/clients/ros2_client ${REPO_DIR}/colcon_ws/src/ros2_client
+ln -sfn ${REPO_DIR}/servers/ros2_server ${REPO_DIR}/colcon_ws/src/ros2_server
 cd ${REPO_DIR}/colcon_ws
 colcon build $@
---
+++ servers/ros2_server/geoscenario_server/geoscenario_server/geoscenario_server.py
@@ -0,0 +1,161 @@
+import rclpy
+from rclpy.node import Node
+from rclpy.executors import ExternalShutdownException
+
+from geoscenario_msgs.msg import Tick, Pedestrian, Vehicle
+from geographic_msgs.msg import GeoPoint
+from lanelet2.io import Origin
+from lanelet2.projection import LocalCartesianProjector
+from lanelet2.core import BasicPoint3d, GPSPoint
+
+class GSServer(Node):
+    def __init__(self):
+        super().__init__('geoscenario_server')
+        # use local+origin=(lat,lon,alt) or WGS84+origin=(0,0,0) coordinates
+        self.declare_parameter('wgs84', False)
+        self.wgs84 = self.get_parameter('wgs84').get_parameter_value().bool_value
+        self.tick_pub = self.create_publisher(Tick, '/gs/tick', 10)
+        self.tick_sub = self.create_subscription(Tick, '/gs/tick_from_client', self.tick_from_client, 10)
+        print(f"GeoScenario server started with wgs84={self.wgs84}")
+
+    def set_msg_pos_vel_from_agent(self, msg, agent):
+        if not self.wgs84:
+            msg.position.x = agent["x"]
+            msg.position.y = agent["y"]
+            msg.position.z = agent["z"]
+            msg.velocity.x = agent["vx"]
+            msg.velocity.y = agent["vy"]
+        else:
+            # convert position to WGS84 coordinates
+            wgs84_point = self.local_cartesian_projector.reverse(
+                BasicPoint3d(agent["x"], agent["y"], agent["z"])
+            )
+            wgs84_vel_point = self.local_cartesian_projector.reverse(
+                BasicPoint3d(agent["x"]+agent["vx"], agent["y"]+agent["vy"], agent["z"])
+            )
+            msg.position.x = wgs84_point.lat
+            msg.position.y = wgs84_point.lon
+            msg.position.z = wgs84_point.ele
+            msg.velocity.x = wgs84_vel_point.lat - wgs84_point.lat
+            msg.velocity.y = wgs84_vel_point.lon - wgs84_point.lon
+
+    def set_agent_pos_vel_from_msg(self, agent, msg):
+        if not self.wgs84:
+            agent["x"] = msg.position.x
+            agent["y"] = msg.position.y
+            agent["z"] = msg.position.z
+            agent["vx"] = msg.velocity.x
+            agent["vy"] = msg.velocity.y
+        else:
+            # convert position from WGS84 to local coordinates
+            local_point = self.local_cartesian_projector.forward(
+                GPSPoint(msg.position.x, msg.position.y, msg.position.z)
+            )
+            local_vel_point = self.local_cartesian_projector.forward(
+                GPSPoint(msg.position.x + msg.velocity.x, msg.position.y + msg.velocity.y, msg.position.z)
+            )
+            agent["x"] = local_point.x
+            agent["y"] = local_point.y
+            agent["z"] = local_point.z
+            agent["vx"] = local_vel_point.x - local_point.x
+            agent["vy"] = local_vel_point.y - local_point.y
+
+    def publish_server_state(self):
+        header, origin, vehicles, pedestrians = # get server state
+
+        tick_count = header["tick_count"]
+
+        tick_msg = Tick()
+        tick_msg.tick_count = tick_count
+        tick_msg.simulation_time = header["simulation_time"]
+        tick_msg.delta_time = header["delta_time"]
+        tick_msg.origin = GeoPoint()
+        if not self.wgs84:
+            tick_msg.origin.latitude = origin["origin_lat"]
+            tick_msg.origin.longitude = origin["origin_lon"]
+            tick_msg.origin.altitude = origin["origin_alt"]
+        else:
+            tick_msg.origin.latitude = 0.0
+            tick_msg.origin.longitude = 0.0
+            tick_msg.origin.altitude = 0.0
+            if self.local_cartesian_projector is None:
+                self.local_cartesian_projector = LocalCartesianProjector(
+                        Origin(origin["origin_lat"], origin["origin_lon"], origin["origin_alt"])
+                    )
+
+        for vehicle in vehicles:
+            msg = Vehicle()
+            msg.id = vehicle["id"]
+            msg.type = vehicle["type"]
+            msg.dimensions.x = vehicle["l"]
+            msg.dimensions.y = vehicle["w"]
+            msg.dimensions.z = vehicle["h"]
+            self.set_msg_pos_vel_from_agent(msg, vehicle)
+            msg.yaw = vehicle["yaw"]
+            msg.steering_angle = vehicle["steering_angle"]
+            tick_msg.vehicles.append(msg)
+
+        for pedestrian in pedestrians:
+            msg = Pedestrian()
+            msg.id = pedestrian["id"]
+            msg.type = pedestrian["type"]
+            msg.dimensions.x = pedestrian["l"]
+            msg.dimensions.y = pedestrian["w"]
+            msg.dimensions.z = pedestrian["h"]
+            self.set_msg_pos_vel_from_agent(msg, pedestrian)
+            msg.yaw = pedestrian["yaw"]
+            tick_msg.pedestrians.append(msg)
+
+        self.tick_pub.publish(tick_msg)
+        self.previous_tick_count = tick_count
+
+
+    def tick_from_client(self, msg):
+        # convert the msg into dictionaries
+        # ignore origin, simulation_time, type, yaw, steering angle not used in the client
+        vehicles = []
+        for msg_vehicle in msg.vehicles:
+            vehicle = {}
+            vehicle["id"] = msg_vehicle.id
+            vehicle["type"] = msg_vehicle.type # not used
+            vehicle["l"] = msg_vehicle.dimensions.x
+            vehicle["w"] = msg_vehicle.dimensions.y
+            vehicle["h"] = msg_vehicle.dimensions.z
+            self.set_agent_pos_vel_from_msg(vehicle, msg_vehicle)
+            vehicle["yaw"] = msg_vehicle.yaw # not used
+            vehicle["steering_angle"] = msg_vehicle.steering_angle # not used
+            vehicle["active"] = msg_vehicle.active
+            vehicles.append(vehicle)
+
+        pedestrians = []
+        for msg_pedestrian in msg.pedestrians:
+            pedestrian = {}
+            pedestrian["id"] = msg_pedestrian.id
+            pedestrian["type"] = msg_pedestrian.type # not used
+            pedestrian["l"] = msg_pedestrian.dimensions.x
+            pedestrian["w"] = msg_pedestrian.dimensions.y
+            pedestrian["h"] = msg_pedestrian.dimensions.z
+            self.set_agent_pos_vel_from_msg(pedestrian, msg_pedestrian)
+            pedestrian["yaw"] = msg_pedestrian.yaw # not used
+            pedestrian["active"] = msg_vehicle.active
+            pedestrians.append(pedestrian)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    gs_server = GSServer()
+
+    try:
+        rclpy.spin(gs_server)
+    except KeyboardInterrupt: # <ctrl>+c
+        gs_server.get_logger().info('Shutdown keyboard interrupt (SIGINT)')
+    except ExternalShutdownException:
+        gs_server.get_logger().info('External shutdown (SIGTERM)')
+    finally:
+        gs_server.destroy_node()
+        rclpy.try_shutdown()
+
+
+if __name__ == '__main__':
+    main()
