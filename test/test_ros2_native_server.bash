950b0b19c5938dfaa02560f7f2883824630a38f0
sdelcore <sdelcore@gmail.com>
2025-11-04 21:31:31 -0500

    add test script for ros2 native server

---
+++ test/test_ros2_native_server.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+shutdown_nodes() {
+    pkill --signal SIGTERM python
+    pkill --signal SIGTERM mock_co_simulat
+    pkill --signal SIGTERM geoscenario_ser
+}
+trap shutdown_nodes SIGINT SIGTERM EXIT
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+REPO_DIR=$(dirname "$SCRIPT_DIR")
+
+cd ${REPO_DIR}
+pixi run ros_client_build_release
+pixi run rqt_topic &
+pixi run ros_mock_co_simulator --ros-args \
+    -p target_delta_time:=1.0 \
+    -p max_simulation_time:=10 \
+    -p real_time_factor:=1.0 &
+
+pixi run ros_native_gss --ros-args \
+    --log-level INFO \
+    -p dashboard_position:="[0.0, 0.0, 960.0, 1080.0]" \
+    -p scenario_files:="['scenarios/long_test_scenarios/gs_ringroad_stress_loop.osm']"
