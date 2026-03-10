52d1bdae71ec6c3e5d368d0925dd138c41b18674
Michał Antkiewicz <michal.antkiewicz@uwaterloo.ca>
2026-03-10 17:57:11 -0400

    rename

--- scripts/opt-geoscenarioserver-install.bash
+++ scripts/opt-geoscenarioserver-install.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SUITE_DIR=$(dirname "$SCRIPT_DIR")
 
 if [ -f /opt/geoscenarioserver/activate.sh ]; then
     echo "GeoScenarioServer found in /opt/geoscenarioserver"
     echo "Remove and re-run the script to download and install the latest version"
 else
     echo "Downloading and installing GeoScenarioServer to /opt/geoscenarioserver"
-    curl -L https://wiselab.uwaterloo.ca/wise-sim/opt-geoscenarioserver-latest.tar.zstd  -o ${SCRIPT_DIR}/opt-geoscenarioserver.tar.zstd
-    tar -I zstdmt -xf ${SCRIPT_DIR}/opt-geoscenarioserver.tar.zstd -C /opt/
-    rm ${SCRIPT_DIR}/opt-geoscenarioserver.tar.zstd
+    NAME=opt-geoscenarioserver-latest.tar.zstd
+    curl -L https://wiselab.uwaterloo.ca/wise-sim/${NAME} -o ${SCRIPT_DIR}/${NAME}
+    tar -I zstdmt -xf ${SCRIPT_DIR}/${NAME} -C /
+    rm ${SCRIPT_DIR}/${NAME}
 fi
