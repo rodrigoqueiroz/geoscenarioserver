33f35a6164b1fe8d4e091fa82a99e2fb3b23aade
Michal Antkiewicz <michal.antkiewicz@uwaterloo.ca>
2026-03-23 20:40:24 -0400

    point to pixi.toml of each ros package

--- scripts/build_all_conda_packages.bash
+++ scripts/build_all_conda_packages.bash
@@ -8,11 +8,11 @@ cd ${REPO_DIR}
 pixi build
 # creates geoscenarioserver-<version>-<build>.conda
 
-pixi build --path ros2/src/geoscenario_msgs
+pixi build --path ros2/src/geoscenario_msgs/pixi.toml
 # Creates ros-humble-geoscenario-msgs-<version>-<build>.conda
 
-pixi build --path ros2/src/geoscenario_server
+pixi build --path ros2/src/geoscenario_server/pixi.toml
 # Creates ros-humble-geoscenario-server-<version>-<build>.conda
 
-pixi build --path ros2/src/geoscenario_client
+pixi build --path ros2/src/geoscenario_client/pixi.toml
 # Creates ros-humble-geoscenario-client-<version>-<build>.conda
